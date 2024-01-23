be12071e1ba888c34a0e0eef98f2be47aeb5e010
ravan <200118rajesh@cosmoscollege.edu.np>
2024-01-23 22:03:45 +0545

    user given sum

---
+++ givensum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class givensum {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+       
+       
+        System.out.println("enter value of a");
+        int a=scanner.nextInt();
+        System.out.println("enter value of b");
+        int b=scanner.nextInt();
+        int sum= a+b;
+        System.out.println(sum);
+
+    }
+}
