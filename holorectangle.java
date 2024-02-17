a858a6ba730840eed893618c780554b7075eff98
ravan <200118rajesh@cosmoscollege.edu.np>
2024-02-17 21:17:27 +0545

    holo rectangle

---
+++ holorectangle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class holorectangle {
+        public static void main(String args[]){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter number of row");
+        int a=scanner.nextInt();
+        System.out.println("enter number of column");
+        int b=scanner.nextInt();
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=b;j++){
+                if(i==1 || i == a || j == 1 || j == b ){
+                System.out.print("*");
+            }
+            else {
+                System.out.print(" ");
+            }
+        }
+            System.out.println ("");
+           
+        }
+
+    }
+}
+
