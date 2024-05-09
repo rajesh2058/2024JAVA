53cf339be3d3e9665d4af0ad9aed036b8562c4af
ravan <200118rajesh@cosmoscollege.edu.np>
2024-05-09 23:28:55 +0545

    compare

---
+++ DSA/string3.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class string3 {
+    public static void main(String[] args) {
+        Scanner A = new Scanner(System.in);
+        System.out.println("enter 1st word");
+        String b = A.next();//whole words
+        //System.out.println(b);
+        System.out.println("enter 2nd word");
+        String c= A.next(); //single word next 
+       // System.out.println(c);
+        // int a =b.length();
+        // int d =c.length();
+    int result = b.compareTo(c); //compare length
+    System.out.println(result);
+    if(b.equals(c)){ //compare words
+        System.out.println("same");
+    }
+    else{
+        System.out.println("different");
+    }
+
+
+    }
+}
