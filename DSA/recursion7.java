f1417b63bf6e2083320d1f63851c7870b5ab02a1
Rajesh shahi <200118rajesh@cosmoscollege.edu.np>
2024-05-20 22:50:52 +0545

    TOH

--- DSA/recursion7.java
+++ DSA/recursion7.java
@@ -1,26 +1,26 @@
 //tower of hanoi
 public class recursion7 {
-    public static void TOH(int n, String source,String helper, String destination) {
-      if(n==1){
-        System.out.println("the "+n+" disk move from "+source+" "+destination);
+    public static void TOH(int r, String source,String helper, String destination) {
+      if(r==1){
+        System.out.println("the "+r+" disk move from "+source+" "+destination);
         return ;
       }
-      System.out.println("the "+n+" disk move from "+source+" "+destination);
+      System.out.println("the "+r+" disk move from "+source+" "+destination);
       //act diff for different call so
-      TOH(n-1, source, destination, helper);
+      TOH(r-1, source, destination, helper);
        
-      TOH(n-1,helper,source,destination);
+      TOH(r-1,helper,source,destination);
    
     }
     public static void main(String[] args) {
-        int n =5 ;
+        int r = 5;
 
-        TOH(n,"s","h","d");
+        TOH(r,"s","h","d");
 
     }
-}
-// TOH(3, "A", "B", "C") calls TOH(2, "A", "C", "B").
+  }
 
+  // TOH(3, "A", "B", "C") calls TOH(2, "A", "C", "B").
 // TOH(2, "A", "C", "B") calls TOH(1, "A", "B", "C").
 // TOH(1, "A", "B", "C"): Base case, move disk 1 from A to C.
 // Return to TOH(2, "A", "C", "B").
