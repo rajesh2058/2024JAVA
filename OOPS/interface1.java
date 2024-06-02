86e5e8c92e5dabbf18e6b58f8fbdd229067a1342
Rajesh shahi <200118rajesh@cosmoscollege.edu.np>
2024-06-02 22:27:11 +0545

    all

---
+++ OOPS/interface1.java
@@ -0,0 +1,27 @@
+
+    interface Animal {
+        void walk();
+     }
+     
+     
+     class Horse implements Animal {
+        public void walk() {
+            System.out.println("Horse walks on 4 legs");
+        }
+     }
+     
+     
+     class Chicken implements Animal {
+        public void walk() {
+            System.out.println("Chicken walks on 2 legs");
+        }
+     }
+     
+     
+     public class interface1 {
+        public static void main(String args[]) {
+           Horse horse = new Horse();
+           horse.walk();
+        }
+     }
+     
---
+++ OOPS/runtimepolymorphism.java
@@ -0,0 +1,25 @@
+class Shape {
+    public void area() {
+        System.out.println("Displays Area of Shape");
+    }
+ }
+ class Triangle extends Shape {
+    public void area(int h, int b) {
+        System.out.println(0.5*b*h);
+    }  
+ }
+ class Circle extends Shape {
+    public void area(int r) {
+        System.out.println((3.14)*r*r);
+    }  
+ }
+ 
+public class runtimepolymorphism {
+    public static void main(String[] args) {
+        Triangle s = new Triangle();
+        s.area();
+        s.area(2,3);
+        Circle c = new Circle();
+        c.area(5);
+    }
+}
