6c7d9cf7d77c64dbdbc84014d489b6477802e790
Rajesh shahi <200118rajesh@cosmoscollege.edu.np>
2024-06-06 22:20:03 +0545

    inheritance

--- OOPS/inheritance.java
+++ OOPS/inheritance.java
@@ -1,5 +1,23 @@
-package OOPS;
 
+class Shape{
+    void area(){
+        System.out.println("area is not right now");
+    }
+}
+class Rectangle extends Shape{
+    int length ;
+    int breath;
+ void area(int length,int breath){
+    this.length = length;
+    this.breath = breath ;
+    int area1 = length * breath ;
+    System.out.println("area is "+area1);
+ }
+}
 public class inheritance {
-    
+    public static void main(String[] args) {
+    Rectangle A = new Rectangle();
+    A.area(5,4);
+    A.area();
+    }
 }
