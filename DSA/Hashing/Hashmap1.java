8f3ee5c529676e06567b666e3480f368dee23357
Rajesh shahi <200118rajesh@cosmoscollege.edu.np>
2024-06-19 00:10:07 +0545

    hashmap and iteration

--- DSA/Hashing/Hashmap1.java
+++ DSA/Hashing/Hashmap1.java
@@ -7,5 +7,30 @@ public class Hashmap1 {
         System.out.println(map);
         map.put("Nepal", 02);
         System.out.println(map);
+        //search
+        if(map.containsKey("Nepal")){
+            System.out.println("value present in data table");
+        }
+        else{
+            System.out.println("value doesnot present in data table");
+        }
+        //iteration ways
+        //i from entry set
+        // for(int val : arr) int become map.entry 
+        //and val become e and arr beome map.entryset
+        //inside e we have all set of data
+        //we access data from e
+        for(Map.Entry<String,Integer> e : map.entrySet()){
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+        }
+        //iteration ii from keyset
+        Set<String> Keys = map.keySet();
+        for(String key : Keys){
+            System.out.println(key+ " " + map.get(key));
+            //remove
+            map.remove("Nepal");
+            System.out.println(map);
+        }
     }
 }
