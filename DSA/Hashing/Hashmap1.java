307d87adbb203a557bcbf105150afd0ee98e5258
Rajesh shahi <200118rajesh@cosmoscollege.edu.np>
2024-06-18 23:51:47 +0545

    hashmap

---
+++ DSA/Hashing/Hashmap1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Hashmap1 {
+    public static void main(String[] args) {
+        HashMap<String,Integer> map = new HashMap<>();
+        //insertion 
+        map.put("Nepal",01);
+        System.out.println(map);
+        map.put("Nepal", 02);
+        System.out.println(map);
+    }
+}
