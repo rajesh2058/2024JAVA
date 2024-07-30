d3fb3f326d58e52014b8f17100aa56d5f45ab08b
Rajesh shahi <200118rajesh@cosmoscollege.edu.np>
2024-07-30 23:41:28 +0545

    wow

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class helloworld{
     public static void main(String args[]){
-        System.out.println("hello world i am rajesh shahi ");
+        System.out.println("hello world i am rajesh shahi yes ");
      
     }
 }
